e0bc0b8b2ae2bea964a8d1d4f1adeec51d62ee4a
lehung2022 <hung.le.programmer@gmail.com>
2023-11-20 10:26:14 +0700

    validation with regular expression mode is done

---
+++ js/number-04.js
@@ -0,0 +1,16 @@
+function is_dateString(str) {
+    regexp = /^(1[0-2]|0?[1-9])\/(3[01]|[12][0-9]|0?[1-9])\/(?:[0-9]{2})?[0-9]{2}$/;
+
+    if (regexp.test(str)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(is_dateString("01/01/2022"));
+
+console.log(is_dateString("01/22/2023"));
+
+console.log(is_dateString("32/01/2023"));
---
+++ js/number-30.js
@@ -0,0 +1,9 @@
+function lower_am_pm(dt) {
+    return dt.getHours() < 12 ? 'AM' : 'PM';
+}
+
+dt = new Date();
+console.log(lower_am_pm(dt));
+
+dt = new Date(1989, 10, 1);
+console.log(lower_am_pm(dt));
